695c3782c24132989518f24c24f7578bdc618cc4
KidWizardOfTheWeb <64866727+KidWizardOfTheWeb@users.noreply.github.com>
2022-07-17 23:02:37 -0400

    Reworked script + manual input added
    For the time being, manual input is being used for finding mesh start and faces start due to lack of pointers to such. I could not find them quite yet, so this is easier to at least get the format working for now. Code will be cleaned up when proper pointers are found. Some information lies below the header and first mesh start, but I'm not 100% on all of the details in it. Will work to research and improve mesh reading as time goes on. Produces output but incorrect output as of this moment. Covers Issue #2 partially, rework eschews loop entirely, and uses manual input to solve the issue temporarily.

--- OKNoesisScripts/Experimental/Ollie King Stages.py
+++ OKNoesisScripts/Experimental/Ollie King Stages.py
@@ -1,6 +1,6 @@
-# Ollie King (Arcade)
+# Ollie King Stages script (Arcade)
 # Noesis script by KC, 2022
-# Last updated: 25 June 2022
+# Last updated: 17 July 2022
 
 # ** WORK IN PROGRESS! **
 
@@ -34,88 +34,112 @@ def bcLoadModel(data, mdlList):
     # txb_file = curr_folder + mesh_name + ".txb"
     mesh_num = 0
 
-    # Changed to manual file choosing for testing
-    txb_file = rapi.loadPairedFileGetPath("texture file", ".txb")
-    while(txb_file):
-        tex_file = NoeBitStream(rapi.loadIntoByteArray(txb_file[1]))
-        mat_list, tex_list = ReadTextures(tex_file)
-        txb_file = rapi.loadPairedFileGetPath("texture file file", ".txb")
-    print("Proceeding to mesh parsing...") # Once complete, display message before attempting to find entry point.
+    # Changed to manual file choosing for testing, removed functionality until texture IDs are found
+    # txb_file = rapi.loadPairedFileGetPath("texture file", ".txb")
+    # while(txb_file):
+    #     tex_file = NoeBitStream(rapi.loadIntoByteArray(txb_file[1]))
+    #     mat_list, tex_list = ReadTextures(tex_file)
+    #     txb_file = rapi.loadPairedFileGetPath("texture file file", ".txb")
+    # print("Proceeding to mesh parsing...") # Once complete, display message before attempting to find entry point.
 
+    meshInfoStart = noesis.userPrompt(noesis.NOEUSERVAL_INT, "Mesh info starting address entry", "Enter the address where the mesh info starts at (about 0x20 before actual mesh start).", "0x0")
+    faceStart = noesis.userPrompt(noesis.NOEUSERVAL_INT, "Face info starting address entry", "Enter the address where the face info starts at (after mesh end).", "0x0")
+    primPrompt = noesis.userPrompt(noesis.NOEUSERVAL_INT, "Primative type entry", "Enter 0 for triangle primatives, or enter 1 for tristrip primatives.", "0")
 
-    # bones = []
-    # bone_num = 0
+    bs.seek(meshInfoStart) # Changed seek to manual input for current usage
+    vert_offset = meshInfoStart + 0x20
+    vert_count = bs.readUInt()
+    print(vert_count)
+    face_count = bs.readUInt()
+    print(face_count)
+    stage_type = bs.readUInt()
+    print(hex(stage_type))
+    extra_info = bs.readUInt()
+    print(hex(extra_info))
+    # Depending on stage type, change vertex padding/stride.
+    if stage_type == 0x0142:
+        stride_value = 0xC
+    elif stage_type == 0x0242:
+        stride_value = 0x14
+    elif stage_type == 0x0112:
+        stride_value = 0x14
+    elif stage_type == 0x0212:
+        stride_value = 0x18
+    # table2 = bs.readUInt()
+    # table2_size = bs.readUInt()
+    # table1 = bs.readUInt()
+    # entries = bs.readUInt()
 
-    # parent_info = [-1, 0, 1, 2, 3, 4, 4, 6, 7, 8, 4, 10, 11, 12, 1, 14, 15, 1, 17, 18, 0]				# default parent info for all skeletons, unneeded
-
-    bs.seek(8) # TODO: figure out where to find the entry point by changing this to seek somewhere else.
-    print(bs.seek(8))
-    table2 = bs.readUInt() + 0x20
-    table2_size = bs.readUInt()
-    table1 = bs.readUInt()
-    entries = bs.readUInt()
-
-    print(table2 - 0x20) # Table sometimes gets populated, print some info on it.
-    print("Entries: " + str(entries)) # No entries happen, which mean loop never starts.
+    # print(table2) # Table sometimes gets populated, print some info on it.
+    # print("Entries: " + str(entries)) # No entries happen, which mean loop never starts.
     
-    print("Loop starts after here.") # Another confirmation because I just gotta know.
-    for a in range(entries):
-        print("Now we're in it.") # And another confirmation because I need to know when I'm in the loop. Haven't gotten in, yet.
-        bs.seek(table2 + (a * 0x20))
-        offset = bs.readUInt() + 0x20
-        offset2 = bs.readUInt() + 0x20
-        tex_num = bs.readUInt()
-        bs.seek(offset)
-        prim_type = bs.readUInt()							# 4 = triangles, 5 = strips
-        entry_type = bs.readUInt()
+    # print("Loop starts after here.") # Another confirmation because I just gotta know.
+    # for a in range(entries): loop removed currently
+    # print("Now we're in it.") # And another confirmation because I need to know when I'm in the loop. Haven't gotten in, yet.
+    # bs.seek(table2 + (a * 0x20))
+    # print(bs.seek(table2 + (a * 0x20)))
+    bs.seek(vert_offset)
+    # offset = bs.readUInt() + 0x20
+    # print(offset)
+    # offset2 = bs.readUInt() + 0x20
+    # tex_num = bs.readUInt()
+    # bs.seek(offset)
+    # prim_type = bs.readUInt()							# 4 = triangles, 5 = strips
+    # entry_type = bs.readUInt()
 
-        # if entry_type == 0x112:							# bone info, don't need it and will remove later.
-        #     bs.seek(offset + 0x20)
-        #     pos = NoeVec3.fromBytes(bs.readBytes(12))
-        #     matrix = NoeAngles([0,0,90]).toMat43()
-        #     pos[2] *= -1 # had to add this recently, something changed with the script and it didn't automatically fix it anymore so this properly flips Z coords of bone position to be on the mesh
-        #     matrix[3] = pos
-        #     bones.append(NoeBone(bone_num, "Bone_" + str(bone_num), matrix, None, parent_info[bone_num]))
-        #     bone_num += 1
-        print("Entry type not found (yet).")
-        if entry_type == 0x142:							# mesh info, stride 0x28, hardcoded one entry type of 42 01 00 00 so far.
-            print("Entry type found.")
-            vert_offset = bs.readUInt() + 0x20
-            vert_count = bs.readUInt()
-            face_offset = bs.readUInt() + 0x20
-            face_count = bs.readUInt()
+    # print("Entry type not found (yet).")
+    # if entry_type == 0x0142:							# mesh info, stride 0x28, changed to user input for the time being. Input the offset where vert_count, face_count, and mesh type is found.
+        # print("Entry type found.")
+        # vert_offset = meshInfoStart + 0x20
+        # vert_count = bs.readUInt()
+        # face_offset = faceStart
+        # face_count = bs.readUInt()
 
-            bs.seek(vert_offset)
-            vertices = bytes()
+        # bs.seek(vert_offset) # vert offset determined by user input
+    vertices = bytes()
+    vertices = bs.readBytes(vert_count * stride_value)
 
-            for v in range(vert_count):
-                vx, vy, vz, unk1 = bs.read("4f")
-                b_idx = bs.read("I")[0]
-                nx, ny, nz = bs.read("3f")
-                uvx, uvy = bs.read("2f")
-                vz = -vz
-                vertices += noePack("ffffIfffff", vx, vy, vz, unk1, b_idx, nx, ny, nz, uvx, uvy)
+    # for v in range(vert_count):
+    #     vx, vy, vz, unk1 = bs.read("4f")
+    #     b_idx = bs.read("I")[0]
+    #     nx, ny, nz = bs.read("3f")
+    #     uvx, uvy = bs.read("2f")
+    #     vz = vz
+    #     vertices += noePack("ffffIfffff", vx, vy, vz, unk1, b_idx, nx, ny, nz, uvx, uvy)
 
-            bs.seek(face_offset)
-            faces = bs.readBytes(face_count * 2)
+    bs.seek(faceStart) # face offset determined by user input
+    faces = bs.readBytes(face_count * 2)
 
-            rapi.rpgSetName(mesh_name + "_" + str(mesh_num))
-            rapi.rpgSetMaterial("Material_" + str(tex_num))
-            rapi.rpgBindPositionBufferOfs(vertices, noesis.RPGEODATA_FLOAT, 0x28, 0)
-            rapi.rpgBindBoneIndexBufferOfs(vertices, noesis.RPGEODATA_UBYTE, 0x28, 0x10, 4)
-            rapi.rpgBindNormalBufferOfs(vertices, noesis.RPGEODATA_FLOAT, 0x28, 0x14)
-            rapi.rpgBindUV1BufferOfs(vertices, noesis.RPGEODATA_FLOAT, 0x28, 0x20)
+    rapi.rpgSetName(mesh_name + "_" + str(mesh_num))
+    rapi.rpgBindPositionBufferOfs(vertices, noesis.RPGEODATA_FLOAT, stride_value, 0)
+    # rapi.rpgBindNormalBufferOfs(vertices, noesis.RPGEODATA_FLOAT, stride_value, 0x14)
+    # rapi.rpgBindUV1BufferOfs(vertices, noesis.RPGEODATA_FLOAT, stride_value, 0x20)
+    # rapi.rpgSetMaterial("Material_" + str(tex_num))
+    # rapi.rpgBindPositionBufferOfs(vertices, noesis.RPGEODATA_FLOAT, 0x28, 0)
+    # # rapi.rpgBindBoneIndexBufferOfs(vertices, noesis.RPGEODATA_UBYTE, 0x28, 0x10, 4)
+    # rapi.rpgBindNormalBufferOfs(vertices, noesis.RPGEODATA_FLOAT, 0x28, 0x14)
+    # rapi.rpgBindUV1BufferOfs(vertices, noesis.RPGEODATA_FLOAT, 0x28, 0x20)
 
-            if prim_type == 4:
-                rapi.rpgCommitTriangles(faces, noesis.RPGEODATA_USHORT, face_count, noesis.RPGEO_TRIANGLE)
+    if primPrompt == 0:
+        rapi.rpgCommitTriangles(faces, noesis.RPGEODATA_USHORT, face_count, noesis.RPGEO_TRIANGLE)
 
-            if prim_type == 5:
-                rapi.rpgCommitTriangles(faces, noesis.RPGEODATA_USHORT, face_count, noesis.RPGEO_TRIANGLE_STRIP)
+        mesh_num += 1
 
-            mesh_num += 1
+    if primPrompt == 1:
+        rapi.rpgCommitTriangles(faces, noesis.RPGEODATA_USHORT, face_count, noesis.RPGEO_TRIANGLE_STRIP)
 
-        else:
-            print("Unknown entry type: ", hex(entry_type))
+        mesh_num += 1
+
+    # if prim_type == 4:
+    #     rapi.rpgCommitTriangles(faces, noesis.RPGEODATA_USHORT, face_count, noesis.RPGEO_TRIANGLE)
+
+    # if prim_type == 5:
+    #     rapi.rpgCommitTriangles(faces, noesis.RPGEODATA_USHORT, face_count, noesis.RPGEO_TRIANGLE_STRIP)
+
+    #     mesh_num += 1
+
+    # else:
+    #     print("Unknown entry type: ", hex(entry_type))
 
 
     try:
@@ -123,7 +147,7 @@ def bcLoadModel(data, mdlList):
     except:
         mdl = NoeModel()
 
-    mdl.setModelMaterials(NoeModelMaterials(tex_list, mat_list))
+    # mdl.setModelMaterials(NoeModelMaterials(tex_list, mat_list))
     # mdl.setBones(bones)
     mdlList.append(mdl)
 
