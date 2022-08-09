1c780468d7813159be0ef69caeaaf03cc46027f4
KidWizardOfTheWeb <64866727+KidWizardOfTheWeb@users.noreply.github.com>
2022-08-09 18:43:37 -0400

    Updated types 3, 5, and 6 with morton order
    Types 3 and 5 were some sort of swizzle I didn't understand, but apparently there is a universal xbox swizzle called morton order built into noesis. Tried it, finally got a coherent image. Type 6 is strange, only obi.txb is type 6 and smaller than all other type 6 images. Made a specific edge case for it just for this reason. Other type 6 images follow the morton order. One texture left to figure out is the 61st one in s6_area02. Solves issue #3, minus that one texture.

--- OKNoesisScripts/Stable/Ollie King Textures.py
+++ OKNoesisScripts/Stable/Ollie King Textures.py
@@ -1,7 +1,7 @@
 # Ollie King (Arcade)
-# Original Noesis script by DKDave, 2021
+# Original Noesis script by Dave, 2021
 # Derived texture section to separate script by KC
-# Last updated: 6 July 2022
+# Last updated: 9 August 2022
 
 # ** WORK IN PROGRESS! **
 
@@ -33,6 +33,7 @@ def bcLoadRGBA(data, tex_list):
     print("Tex count: ", tex_count)
 
     offset = 0x20
+    file_name = rapi.getLocalFileName(rapi.getInputName()).lower()
 
     for a in range(tex_count):
         tex1 = None # Added because certain textures throw an error based on assigning too early.
@@ -45,7 +46,7 @@ def bcLoadRGBA(data, tex_list):
         bs.seek(offset + 0x20)
         raw_image = bs.readBytes(data_size)
 
-        
+
         if img_type == 7:
             tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_DXT1)
 
@@ -56,25 +57,29 @@ def bcLoadRGBA(data, tex_list):
             raw_image = rapi.imageDecodeRaw(raw_image, width, width, "b5g5r5p1")
             tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_RGBA32)
 
-        elif img_type == 4:										
+        elif img_type == 4:					
             raw_image = rapi.imageDecodeRaw(raw_image, width, width, "b4g4r4a4")
             tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_RGBA32)
-            
+
         elif img_type == 9: # Similar to 8, listed as different image type
             tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_DXT5)
-        
-        elif img_type == 6: # A single image uses this: obi.txb
-            raw_image = rapi.imageDecodeRaw(raw_image, width, width, "r8g8b8a8")
-            tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_RGBA32)
 
-        elif img_type == 3: # A single image uses this: s6_area02.txb
-            # TODO: Confirm if this is the proper RGBA format. This is the closest so far. The 60th image in the file uses this format specifically.
-            raw_image = rapi.imageDecodeRaw(raw_image, width, width, "b5g5r5p1")
-            tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_RGBA32)
+        elif img_type == 6: # Used for first image of s3_tenkyu and obi. They are different formats however, so edgecase is used.
+            if file_name == "obi.txb":
+                raw_image = rapi.imageDecodeRaw(raw_image, width, width, "r8g8b8a8")
+                tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_RGBA32)
+            else:
+                raw_image = rapi.imageFromMortonOrder(raw_image, width, width)
+                tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_RGBA32)
 
-        elif img_type == 5: # Strange format, closest is r8g8_uint in raw texture cooker/b8g8r8a8, might be another format. 
+        elif img_type == 3: # Xbox Morton order.
+            # TODO: Confirm if this is the proper DXT format. 
+            raw_image = rapi.imageFromMortonOrder(raw_image, width, width)
+            tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_DXT5)
+
+        elif img_type == 5: # Xbox Morton order.
         # Used in London stages and Kyoto skyboxes: s2_area16.txb, s5_area02.txb, s5_area18.txb, s3_tenkyu.txb, s6_tenkyu.txb
-            raw_image = rapi.imageDecodeRaw(raw_image, width, width, "b8g8r8a8")
+            raw_image = rapi.imageFromMortonOrder(raw_image, width, width)
             tex1 = NoeTexture("Texture_" + str(a) + ".bmp", width, width, raw_image, noesis.NOESISTEX_RGBA32)
 
         else:
